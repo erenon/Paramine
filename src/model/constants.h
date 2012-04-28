396d22905fe3bdfde5c51e1d366c2f380c2b00fc
erenon <erenon2@gmail.com>
2012-04-28 18:18:34 +0200

    Constants adjusted

--- src/model/constants.h
+++ src/model/constants.h
@@ -5,7 +5,17 @@ typedef float Real;
 
 #define STATION_COUNT 5
 #define LANE_COUNT 4
-#define BOGIE_SWITCH_COUNT 50000000
-#define ROCK_AMOUNT 500000000
+
+/*
+ * Total switch count in the mine
+ * equals to M * LANE_COUNT
+ */
+#define BOGIE_SWITCH_COUNT 40000000
+
+/**
+ * Initial rock amount on each station
+ * equals to M * 10
+ */
+#define ROCK_AMOUNT 100000000
 
 #endif /* SRC_MODEL_CONSTANTS_H_ */
