d1eb0bb32ca431f40ac58d4965193d07a2932056
erenon <erenon2@gmail.com>
2012-05-01 22:23:46 +0200

    AtomicStation: ability to use lock added

--- src/model/atomic_station.cc
+++ src/model/atomic_station.cc
@@ -1,21 +1,29 @@
 #include "atomic_station.h"
 
-#include <omp.h>
-
 namespace Model {
 
 AtomicStation::AtomicStation(int bogieId, Real rockAmount)
     :Station(bogieId, rockAmount)
-{}
+{
+    omp_init_lock(&rockLock);
+}
 
 void AtomicStation::incrementRockAmount() {
+    //omp_set_lock(&rockLock);
     #pragma omp atomic
-    this->rockAmount++;
+    rockAmount++;
+    //omp_unset_lock(&rockLock);
 }
 
 void AtomicStation::decrementRockAmount() {
+    //omp_set_lock(&rockLock);
     #pragma omp atomic
-    this->rockAmount--;
+    rockAmount--;
+    //omp_unset_lock(&rockLock);
+}
+
+AtomicStation::~AtomicStation() {
+    omp_destroy_lock(&rockLock);
 }
 
 }    // namespace
--- src/model/atomic_station.h
+++ src/model/atomic_station.h
@@ -3,16 +3,22 @@
 
 #include "station.h"
 
+#include <omp.h>
+
 namespace Model {
 
 using ::Model::Station;
 
 class AtomicStation :public Station {
+private:
+    omp_lock_t rockLock;
 public:
     AtomicStation(int bogieId, Real rockAmount);
 
     void incrementRockAmount();
     void decrementRockAmount();
+
+    ~AtomicStation();
 };
 
 }    // namespace
